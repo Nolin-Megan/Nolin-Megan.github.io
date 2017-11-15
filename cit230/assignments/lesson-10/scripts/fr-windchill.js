857ef0e40a509ab85c5048c828f537f1c9021f08
Nolin-Megan <31864170+Nolin-Megan@users.noreply.github.com>
2017-11-14 17:43:11 -0700

    ikhvjuh

---
+++ cit230/assignments/lesson-10/scripts/fr-windchill.js
@@ -0,0 +1,12 @@
+//f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 
+
+function windchill(){
+    var t = (90 + 66) / 2;
+    var s = 5;
+    var part = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .16) + 0.4275 * t * Math.pow(s, .16);
+    var f = Math.round(part * 100) / 100
+    
+    document.getElementById("outputs").innerHTML = f
+}
+
+windchill();
